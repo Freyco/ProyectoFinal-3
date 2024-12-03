4c08c8bbd552027229422c924f8cb8a6d2fffe76
Freyco <frrodriguezco@est.utn.ac.cr>
2024-12-02 22:01:43 -0600

    Subir

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Config : MonoBehaviour
+{
+    public void IrALaPantallaDeJuego()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+
+
